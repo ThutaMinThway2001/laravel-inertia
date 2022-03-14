70034f145550272bd14a670183c375e34a2dc867
Thuta <thutaminthway2001@gmail.com>
2022-03-15 00:23:53 +0630

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="home">
+        <h1>Hello {{name}}</h1>
+
+        <p>In This series, we will use the following frameworks.</p>
+        <ul >
+            <li v-for="framework of frameworks" :key="framework">
+                {{framework}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+}
+</script>
